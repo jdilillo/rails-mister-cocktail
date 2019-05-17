2deb0ff20a7d6bd3c2669e363ee2cedd028682d4
Joe Di Lillo <jdilillo@gmail.com>
2019-05-17 11:24:52 -0400

    Fix typo

--- Gemfile
+++ Gemfile
@@ -32,6 +32,8 @@ gem 'autoprefixer-rails'
 gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'dotenv-rails', groups: [:development, :test]
+gem 'cloudinary', '~> 1.9.1'
+
 
 
 # Use ActiveStorage variant
--- db/migrate/20190516173010_create_ingredients.rb
+++ db/migrate/20190516173010_create_ingredients.rb
@@ -1,4 +1,4 @@
-xclass CreateIngredients < ActiveRecord::Migration[5.2]
+class CreateIngredients < ActiveRecord::Migration[5.2]
   def change
     create_table :ingredients do |t|
       t.string :name
